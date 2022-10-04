581d3fbf06c015d923b26e363fd472f7f48c33ca
Sohail Ishaque <sohail.sohailishaq@gmail.com>
2022-10-04 19:35:27 +0500

    04 complete

---
+++ 04/index.js
@@ -0,0 +1,6 @@
+//First Variable
+const author = "James Cameron";
+const quote = `"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."`;
+
+//Output
+document.write("<h2>", author, ", ", quote, "</h2>");
