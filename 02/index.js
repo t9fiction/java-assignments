39e979902482b24df63e0c1821b7cf68a420bda2
Sohail Ishaque <sohail.sohailishaq@gmail.com>
2022-10-04 18:09:23 +0500

    assignment 2 complete

---
+++ 02/index.js
@@ -0,0 +1,9 @@
+  //First Variable
+  const myName = "Sohail Ishaque";
+
+  //Second Variable
+  const message = "Are you dreaming to become a Metaverse Guru ?";
+
+  //Output
+  document.write("<p>Hello ",myName,"</p>");
+  document.write(message);
