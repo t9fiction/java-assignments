095ab3f7bccfa309cc319be75d9e82b12cf64f24
Sohail Ishaque <sohail.sohailishaq@gmail.com>
2022-10-04 22:17:58 +0500

    07

---
+++ 07/index.js
@@ -0,0 +1,7 @@
+let var1 = 4;
+
+//Output
+document.getElementById("add").innerHTML = "Addition : ".concat(var1 + 4);
+document.getElementById("sub").innerHTML = "Subtraction : ".concat(12 - var1);
+document.getElementById("mul").innerHTML = "Multiplication : ".concat(var1 * 2);
+document.getElementById("dvd").innerHTML = "Division : ".concat(32 / var1);
