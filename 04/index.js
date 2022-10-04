d4ba42ea300369681073c0b434196de1c43c38e8
Sohail Ishaque <sohail.sohailishaq@gmail.com>
2022-10-04 21:30:38 +0500

    05 complete

--- 04/index.js
+++ 04/index.js
@@ -1,6 +1,2 @@
-//First Variable
-const author = "James Cameron";
-const quote = `"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success."`;
-
 //Output
-document.write("<h2>", author, ", ", quote, "</h2>");
+document.write("<h3>James Cameron, If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.</h3>");
