f56d3bc09b2a5e65e15113cdb92d18a31b75ad7e
Sohail Ishaque <sohail.sohailishaq@gmail.com>
2022-10-04 21:51:05 +0500

    06

---
+++ 06/index.js
@@ -0,0 +1,6 @@
+//First Variable
+const person = "     Sohail Ishaque     ";
+
+//Output
+document.write("<h3>", person, "\n \n <br/>", person, "</h3>");
+document.write("<h3>", person, "\t", person, "</h3>");
